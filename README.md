89cf7427edbc1c1538dda6c12ceb9477a560b5fa
Spocke <spocke@moxiecode.com>
2013-04-30 10:36:13 +0200

    Added basic parser, cli and unit tests

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 moxiedoc
 ========
+
+{
+	"namespace.Class": {
+		"type": "class|enum|struct|mixin|interface",
+		"members": [
+			{
+				"type": "method|field|property|event|constant|callback",
+				"static": true,
+				"abstract": true,
+				"name": "doStuff",
+				"access": "private|protected|internal|public,
+				"extends": "namespace.Class",
+				"implements": ["namespace.Interface"],
+				"deprecated": "Text about deprecation",
+				"description": "Do stuff",
+				"mixes": ["namespace.Class1", "namespace.Class2"],
+				"examples": [
+					{text: "Some example"},
+					{caption: "Some example", text: "Some example"}
+				],
+				"params": [
+					{"name": "mystuff", type": "string", "description": "My stuff"}
+				],
+				"returns": {
+					{type: "string", "description": "Returns string."}
+				}
+			}
+		]
+	}
+}
---
+++ test/test-parser.js
@@ -0,0 +1,256 @@
+var Parser = require('../lib/moxiedoc').Parser;
+
+exports.testInit = function(test) {
+	var parser = new Parser({});
+
+	test.equals("object", typeof(parser));
+	test.ok(parser instanceof Parser);
+	test.done();
+};
+
+exports.parseNoDocComments = function(test) {
+	var start = 0, tag = 0, end = 0;
+
+	var parser = new Parser({
+		start: function() {
+			start++;
+		},
+
+		tag: function() {
+			tag++;
+		},
+
+		end: function() {
+			end++;
+		}
+	});
+
+	parser.parse("something");
+
+	test.equals(0, start);
+	test.equals(0, tag);
+	test.equals(0, end);
+
+	test.done();
+};
+
+exports.parseStartSingleLine = function(test) {
+	var start = 0, tag = 0, end = 0;
+	var startText;
+
+	var parser = new Parser({
+		start: function(text) {
+			start++;
+			startText = text;
+		},
+
+		tag: function() {
+			tag++;
+		},
+
+		end: function() {
+			end++;
+		}
+	});
+
+	parser.parse([
+		"/**",
+		"* a",
+		"*/"
+	].join('\n'));
+
+	test.equals(1, start);
+	test.equals("a", startText);
+	test.equals(0, tag);
+	test.equals(1, end);
+
+	test.done();
+};
+
+exports.parseStartMultipleLines = function(test) {
+	var start = 0, tag = 0, end = 0;
+	var startText, startInfo;
+
+	var parser = new Parser({
+		start: function(text, info) {
+			start++;
+			startText = text;
+			startInfo = info;
+		},
+
+		tag: function() {
+			tag++;
+		},
+
+		end: function() {
+			end++;
+		}
+	});
+
+	parser.parse([
+		"/**",
+		"* a",
+		"* b",
+		"* c",
+		"*/"
+	].join('\n'));
+
+	test.equals(1, start);
+	test.equals("a\nb\nc", startText);
+	test.equals(0, tag);
+	test.equals(1, end);
+	test.equals(0, startInfo.line);
+
+	test.done();
+};
+
+exports.parseTagSingleLine = function(test) {
+	var start = 0, tag = 0, end = 0;
+	var tagName, tagText, tagInfo;
+
+	var parser = new Parser({
+		start: function() {
+			start++;
+		},
+
+		tag: function(name, text, info) {
+			tag++;
+			tagName = name;
+			tagText = text;
+			tagInfo = info;
+		},
+
+		end: function() {
+			end++;
+		}
+	});
+
+	parser.parse([
+		"/**",
+		"* @a b",
+		"*/"
+	].join('\n'));
+
+	test.equals(1, start);
+	test.equals("a", tagName);
+	test.equals("b", tagText);
+	test.equals(1, tag);
+	test.equals(1, end);
+	test.equals(1, tagInfo.line);
+
+	test.done();
+};
+
+exports.parseTagMultipleLines = function(test) {
+	var start = 0, tag = 0, end = 0;
+	var tagName, tagText;
+
+	var parser = new Parser({
+		start: function() {
+			start++;
+		},
+
+		tag: function(name, text) {
+			tag++;
+			tagName = name;
+			tagText = text;
+		},
+
+		end: function() {
+			end++;
+		}
+	});
+
+	parser.parse([
+		"/**",
+		"* @a b",
+		"* c",
+		"* d",
+		"*/"
+	].join('\n'));
+
+	test.equals(1, start);
+	test.equals("a", tagName);
+	test.equals("b\nc\nd", tagText);
+	test.equals(1, tag);
+	test.equals(1, end);
+
+	test.done();
+};
+
+exports.parseTagSingleLineAfterText = function(test) {
+	var start = 0, tag = 0, end = 0;
+	var tagName, tagText, tagInfo;
+
+	var parser = new Parser({
+		start: function() {
+			start++;
+		},
+
+		tag: function(name, text, info) {
+			tag++;
+			tagName = name;
+			tagText = text;
+			tagInfo = info;
+		},
+
+		end: function() {
+			end++;
+		}
+	});
+
+	parser.parse([
+		"/**",
+		"* text",
+		"* @a b",
+		"*/"
+	].join('\n'));
+
+	test.equals(1, start);
+	test.equals("a", tagName);
+	test.equals("b", tagText);
+	test.equals(1, tag);
+	test.equals(1, end);
+	test.equals(2, tagInfo.line);
+
+	test.done();
+};
+
+exports.parseStartTextAndTagWithEvents = function(test) {
+	var start = 0, tag = 0, end = 0;
+	var tagName, startText, tagText, tagInfo;
+
+	var parser = new Parser();
+
+	parser.on('start', function(text) {
+		start++;
+		startText = text;
+	});
+
+	parser.on('tag', function(name, text, info) {
+		tag++;
+		tagName = name;
+		tagText = text;
+		tagInfo = info;
+	});
+
+	parser.on('end', function() {
+		end++;
+	});
+
+	parser.parse([
+		"/**",
+		"* text",
+		"* @a b",
+		"*/"
+	].join('\n'));
+
+	test.equals(1, start);
+	test.equals("a", tagName);
+	test.equals("b", tagText);
+	test.equals(1, tag);
+	test.equals(1, end);
+	test.equals(2, tagInfo.line);
+
+	test.done();
+};
