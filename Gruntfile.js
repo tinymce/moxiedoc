cd0d7144ce13b95dab4dc7b6b9ed96152b4776d8
Spocke <spocke@moxiecode.com>
2014-09-29 10:31:33 +0200

    Added grunt and fixed unit test framework dependency.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		nodeunit: {
+			all: ['test/**/*.js']
+		},
+		
+		watch: {
+			test: {
+				options: {
+					spawn: false
+				},
+
+				files: ["lib/**/*.js", "test/**/*.js"],
+				tasks: ["nodeunit"]
+			}
+		}
+	});
+
+	require("load-grunt-tasks")(grunt);
+
+	grunt.registerTask("default", ["nodeunit"]);
+};
