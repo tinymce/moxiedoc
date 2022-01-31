c939a839c6637c32651c5a95a622c0079f11f4de
Mike Chau <mike.chau@tiny.cloud>
2022-01-31 17:41:12 +1000

    DOC-1541: working off existing api_members.html template to generate html from json (convert to asciidoc after)

--- templates/antora/antora.converter.js
+++ templates/antora/antora.converter.js
@@ -1,12 +1,29 @@
+const { passThroughOptions } = require("commander");
+
 module.exports = function () {
 
-  function convert(data) {
-    return data.map(function (page) {
+  // Todo we can pass this through later
+  var baseurl = '';
+
+  function hasValue (x) {
+    // empty helper for strings, objects, arrays
+    if (typeof(x) === 'string' || typeof(x) === 'array') {
+      return x.length > 0
+    } else if (typeof(x) === 'object') {
+      return Object.keys(x).length > 0
+    } else {
+      return false;
+    }
+  };
+
+
+  function convert(memberData) {
+    return memberData.map(function (page) {
       // page[0] is json
       // page[1] is adoc
 
-      var json = JSON.parse(page[0].content);
-      var tmp = page[1];
+      var data = JSON.parse(page[0].content);
+      var tmp = page[1].content;
 
       // data.datapath = data.type + '_' + data.fullName.replace(/\./g, '_').toLowerCase();
       // data.desc = data.desc.replace(/\n/g, ' ');
@@ -22,7 +39,24 @@ module.exports = function () {
 
 
       // summary
+      if (hasValue(data.summary)) {
+        tmp += '<p>' + data.summary + '</p>' + '\n'
+      }
+
       // borrows
+      if (hasValue(data.borrows)) {
+        // has not been tested (no borrows/extends data)
+        tmp += '<a class="anchor" id="extends"></a>' + '\n'
+        tmp += '<h2><a class="anchorable" href="#extends">Extends</a></h2>' + '\n'
+
+        data.borrows.forEach(item => {
+          tmp += '<a href="' + baseurl + '/api/' + item + '">' + item + '</a>' + '\n'
+        });
+      }
+
+
+
+
       // examples
       // settings
       // properties
@@ -38,6 +72,9 @@ module.exports = function () {
 
 
       // console.log('****************', page[0].content);
+
+      page[0].content = JSON.stringify(data);
+      page[1].content = tmp
 
       return page;
     })
